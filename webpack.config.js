58603374d9af09c868cb723385719e47b2e3d126
nikitausaty <nikitausatiy@yandex.ru>
2022-11-15 21:38:53 +0300

    confid webpack

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './index.ts'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name][contenthash].bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8900,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /.(scss|css)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json', '.svg'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'race',
+      template: path.resolve(__dirname, './index.html'),
+      filename: 'index.html',
+      // favicon: path.resolve(__dirname, './assets/favicon.png'),
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './assets/*.svg'),
+          to: '[name][ext]',
+        },
+      ],
+    }),
+  ],
+}
