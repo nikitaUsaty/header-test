59e6ba4c3af2276b0e2696a81d24481be245a1e3
nikitausaty <nikitausatiy@yandex.ru>
2022-11-16 00:54:23 +0300

    add req and popup respond

--- index.ts
+++ index.ts
@@ -3,3 +3,30 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+
+const form = document.getElementById('form')
+const message = document.querySelector('.message')
+const showMes = document.getElementById('popup')
+const closeBtn = document.getElementById('close')
+
+form?.addEventListener('submit', () => {
+  handleReq()
+})
+
+async function getReq(): Promise<any> {
+  const response = await fetch('http://ptsv2.com/t/bla/post')
+
+  if (response.status === 200) {
+    let text = await response.text()
+    return text
+  }
+}
+
+async function handleReq() {
+  getReq().then((res) => (message!.innerHTML = res))
+  showMes?.classList.remove('display')
+}
+
+closeBtn?.addEventListener('click', () => {
+  showMes?.classList.add('display')
+})
